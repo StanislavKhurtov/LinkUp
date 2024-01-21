2050d4b42f85a71b53f3375184d99fd179dc44e1
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-21 20:46:48 +0300

    feat: add store

---
+++ src/components/ui/footer/footer.tsx
@@ -0,0 +1,11 @@
+import { Page } from '@/components/ui'
+
+import s from './footer.module.scss'
+
+export const Footer = () => {
+  return (
+    <Page>
+      <div className={s.footer}>2023 Stanislav khurtov</div>
+    </Page>
+  )
+}
