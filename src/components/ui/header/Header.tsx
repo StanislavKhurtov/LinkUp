cae2843d2409d7118a66d90e7b7a70409db72e40
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-22 20:10:13 +0300

    feat: change structure routing

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,12 +1,14 @@
+import { Page } from '@/components/ui'
+
 import h from './Header.module.scss'
 
 export const Header = () => {
   return (
-    <header className={h.header}>
-      <div className={h.header__container}>
+    <Page>
+      <header className={h.header}>
         <div className={'header__logo'}>Logo</div>
         <button className={'header__login'}>Login</button>
-      </div>
-    </header>
+      </header>
+    </Page>
   )
 }
