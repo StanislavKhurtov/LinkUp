1a0260a4862d1ca60c15e0c20985acdc50832a50
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-22 20:11:56 +0300

    feat: change structure routing

--- src/components/ui/layout/layout.tsx
+++ src/components/ui/layout/layout.tsx
@@ -1,17 +1,20 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
+import { Aside } from '@/components/ui/aside/aside'
 import { Footer } from '@/components/ui/footer/footer'
 import { Header } from '@/components/ui/header/Header'
-
 
 type Props = ComponentPropsWithoutRef<'div'>
 export const Layout = forwardRef<ElementRef<'div'>, Props>((props, ref) => {
   const { children, className, ...rest } = props
 
   return (
-    <div ref={ref} {...rest}>
+    <div ref={ref} {...rest} className={'wrapper'}>
       <Header />
-      <div className={'main'}>{children}</div>
+      <main className={'main'}>
+        <Aside />
+        {children}
+      </main>
       <Footer />
     </div>
   )
