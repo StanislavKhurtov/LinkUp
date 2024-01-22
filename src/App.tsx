9c05b2191ec10019dcad6a189ef93c115b7057b3
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-22 20:09:14 +0300

    feat: change structure routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import { Provider } from 'react-redux'
 
-import { Layout } from '@/components/ui/layout/layout'
 import { Router } from '@/router'
 import { store } from '@/services/store'
 
 export function App() {
   return (
     <Provider store={store}>
-      <Layout>
-        <Router />
-      </Layout>
+      <Router />
     </Provider>
   )
-}
-
-{
-  /*  <Player srcList={[]} />
-      <div>Hello</div>
-      <Button as={'a'} href={'http://yandex.ru'} rel={'noopener noreferrer'} target={'_blank'}>
-        Link
-      </Button>*/
 }
