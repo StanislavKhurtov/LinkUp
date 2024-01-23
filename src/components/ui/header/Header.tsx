d52a9a9546b0adaa48a9d5a24aceaf48a7ac4b27
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-23 21:44:00 +0300

    feat: change structure

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,12 +1,21 @@
-import { Page } from '@/components/ui'
+import { Link } from 'react-router-dom'
 
-import h from './Header.module.scss'
+import { Button, Page } from '@/components/ui'
+
+import s from './Header.module.scss'
+
+import logo from '../../../assets/image/logo.png'
 
 export const Header = () => {
   return (
     <Page>
-      <header className={h.header}>
-        <div className={'header__logo'}>Logo</div>
+      <header className={s.header}>
+        <Button as={Link} className={s.header__btn} to={'/'}>
+          <div className={s.header__logo}>
+            <img alt={'image-logo'} className={s.header__image} src={logo} />
+          </div>
+          LinkUp
+        </Button>
         <button className={'header__login'}>Login</button>
       </header>
     </Page>
