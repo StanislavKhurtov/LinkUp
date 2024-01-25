2a2419b6748e8c393b34a6040bf1a1bd7ea3bc3a
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-25 19:28:21 +0300

    feat: change structure profile

---
+++ src/services/profile/profile.service.ts
@@ -0,0 +1,61 @@
+import { baseApi } from '@/services/baseApi'
+import { CreateProfileArgs, ProfileType } from '@/services/profile/profile.types'
+
+import { ResponseType } from '../baseApi/baseApi.types'
+
+const profileService = baseApi.injectEndpoints({
+  endpoints: builder => {
+    return {
+      createProfile: builder.mutation<ResponseType, CreateProfileArgs>({
+        invalidatesTags: ['Profile'],
+        query: args => ({
+          body: args,
+          method: 'PUT',
+          url: 'profile',
+        }),
+      }),
+      createProfilePhoto: builder.mutation<ResponseType, { image: string }>({
+        invalidatesTags: ['Profile'],
+        query: args => ({
+          body: args.image,
+          method: 'PUT',
+          url: 'profile/photo',
+        }),
+      }),
+      createProfileStatus: builder.mutation<ResponseType, { status: string }>({
+        invalidatesTags: ['Profile'],
+        query: args => ({
+          body: args.status,
+          method: 'PUT',
+          url: 'profile/status',
+        }),
+      }),
+      getProfileById: builder.query<ProfileType, { userId: string }>({
+        providesTags: ['Profile'],
+        query: ({ userId }) => {
+          return {
+            url: `profile/${userId}`,
+          }
+        },
+      }),
+      getProfileStatusById: builder.query<any, { userId: string }>({
+        providesTags: ['Profile'],
+        query: ({ userId }) => {
+          return {
+            url: `profile/status/${userId}`,
+          }
+        },
+      }),
+    }
+  },
+})
+
+const {} = profileService
+
+/*
+useCreateProfileMutation,
+useCreateProfilePhotoMutation,
+useCreateProfileStatusMutation,
+useGetProfileByIdQuery,
+useGetProfileStatusByIdQuery,
+*/
