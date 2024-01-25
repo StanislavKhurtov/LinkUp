61e220e997b82c4cf014670779c41cea669a3582
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-25 19:29:32 +0300

    feat: change structure security.service

---
+++ src/services/security/security.service.ts
@@ -0,0 +1,18 @@
+import { baseApi } from '@/services/baseApi'
+
+const securityService = baseApi.injectEndpoints({
+  endpoints: builder => {
+    return {
+      getDSecurity: builder.query<{ url: string }, void>({
+        providesTags: ['Security'],
+        query: () => {
+          return {
+            url: 'security/get-captcha-url',
+          }
+        },
+      }),
+    }
+  },
+})
+
+const {} = securityService
