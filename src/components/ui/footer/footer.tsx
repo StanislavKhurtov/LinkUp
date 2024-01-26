9b1b7698a884ce10e64b088cd9c6a89b3e59925b
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-26 21:39:21 +0300

    feat: change structure footer

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -5,7 +5,7 @@ import s from './footer.module.scss'
 export const Footer = () => {
   return (
     <Page>
-      <footer className={s.footer}>2023 Stanislav khurtov</footer>
+      <footer className={s.footer}>©2023 Stanislav khurtov</footer>
     </Page>
   )
 }
