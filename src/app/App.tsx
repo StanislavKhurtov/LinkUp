bfb4ccda4a28d2b5b0b6d242b0a1af9d745cdccf
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-06 19:34:39 +0300

    feat: change structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux'
 
-import { Router } from '@/app/router'
+import { Router } from '@/app/provider/router'
 import { store } from '@/services/store'
 
 export function App() {
