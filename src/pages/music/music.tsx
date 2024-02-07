ba6dfb92f21606c2f8c7f7f1dac043b3f0c9e022
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-07 20:48:49 +0300

    refactor: change structure music

--- src/pages/music/music.tsx
+++ src/pages/music/music.tsx
@@ -4,6 +4,7 @@ const radio = [
   'https://ice-the.musicradio.com/CapitalXTRANationalMP3',
   'https://dorognoe.hostingradio.ru:8000/dorognoe',
   'http://listen.rpfm.ru:9000/premium128',
+  'http://stream.nonstopplay.co.uk/nsp-128k-mp3',
 ]
 
 export const Music = () => {
