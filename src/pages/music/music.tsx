19d3f5c3c5f06b46870608fa0b9878546c5c60dc
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-07 20:48:37 +0300

    refactor: change structure music

--- src/pages/music/music.tsx
+++ src/pages/music/music.tsx
@@ -1,7 +1,6 @@
 import { Player } from '@/components/ui/audioPlayer/Player'
 
 const radio = [
-  'http://stream.nonstopplay.co.uk/nsp-128k-mp3',
   'https://ice-the.musicradio.com/CapitalXTRANationalMP3',
   'https://dorognoe.hostingradio.ru:8000/dorognoe',
   'http://listen.rpfm.ru:9000/premium128',
