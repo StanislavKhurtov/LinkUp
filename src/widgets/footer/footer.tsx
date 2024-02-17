7ad4baa8f3e208aa1682c1206dab0d40ec7b132b
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-17 12:04:08 +0300

    feat: add i18n lang footer

--- src/widgets/footer/footer.tsx
+++ src/widgets/footer/footer.tsx
@@ -1,4 +1,6 @@
-import { Block } from '@/components/ui'
+import { useTranslation } from 'react-i18next'
+
+import { Block, Button } from '@/components/ui'
 import { useMeQuery } from '@/pages/auth/api'
 
 import s from './footer.module.scss'
@@ -6,12 +8,41 @@ import s from './footer.module.scss'
 export const Footer = () => {
   const { data } = useMeQuery()
   const isAuthenticated = data?.resultCode === 1
+  const { i18n } = useTranslation()
+  const currentLanguage = i18n.language
+
+  const handleClickEN = () => {
+    i18n.changeLanguage('en')
+  }
+  const handleClickRU = () => {
+    i18n.changeLanguage('ru')
+  }
 
   return (
     <>
       {isAuthenticated && (
         <Block as={'footer'} className={s.footer}>
-          <div className={s.footer__text}>©2024 Stanislav Khurtoff</div>
+          <div className={s.footer__block}>
+            <div className={s.footer__text}>©2024 Stanislav Khurtoff</div>
+            <div className={s.footer__lang}>
+              <Button
+                className={currentLanguage === 'en' ? s.active : ''}
+                onClick={handleClickEN}
+                type={'button'}
+                variant={'primary'}
+              >
+                English
+              </Button>
+              <Button
+                className={currentLanguage === 'ru' ? s.active : ''}
+                onClick={handleClickRU}
+                type={'button'}
+                variant={'primary'}
+              >
+                Русский
+              </Button>
+            </div>
+          </div>
         </Block>
       )}
     </>
