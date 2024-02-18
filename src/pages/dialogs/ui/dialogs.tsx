c58feb9c7e2e0fa82cf3ae27cec9137b4eb5cb56
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-18 22:46:31 +0300

    feat: add Dialog

--- src/pages/dialogs/ui/dialogs.tsx
+++ src/pages/dialogs/ui/dialogs.tsx
@@ -1,3 +1,43 @@
+import { NavLink } from 'react-router-dom'
+
+import NoImage from '@/assets/image/noImage.jpg'
+import { Linear } from '@/components/ui'
+import { useGetDialogsQuery } from '@/pages/dialogs/api'
+
+import s from './dialogs.module.scss'
+
 export const Dialogs = () => {
-  return <div>Dialog</div>
+  const { data, isLoading } = useGetDialogsQuery()
+
+  if (isLoading) {
+    return <Linear />
+  }
+
+  return (
+    <div className={s.dialog}>
+      {data?.map(dialog => {
+        return (
+          <div className={s.dialog__item} key={dialog.id}>
+            <div className={s.dialog__logo}>
+              <NavLink to={`/profile/${dialog.id}`}>
+                <img
+                  alt={'image logo'}
+                  className={s.dialog__image}
+                  src={dialog.photos.small !== null ? dialog.photos.small : NoImage}
+                />
+              </NavLink>
+            </div>
+            <div className={s.dialog__body}>
+              <div className={s.dialog__info}>
+                <NavLink className={s.dialog__name} to={`/profile/${dialog.id}`}>
+                  {dialog.userName}
+                </NavLink>
+              </div>
+              <div className={s.dialog__message}>{dialog.lastUserActivityDate}</div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
