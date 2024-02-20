aa9e859244f6591ef8efc1fd47f2de87f4e220b7
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-20 14:17:38 +0300

    feat: change structure

--- src/pages/dialogs/api/dialog.types.ts
+++ src/pages/dialogs/api/dialog.types.ts
@@ -12,3 +12,49 @@ export type DialogPhotos = {
   large: string
   small: string
 }
+
+export type UserType = {
+  aboutMe: string
+  contacts: UserContacts
+  fullName: string
+  lookingForAJob: boolean
+  lookingForAJobDescription: string
+  photos: UserPhotos
+  userId: number
+}
+export type UserContacts = {
+  facebook: string
+  github: string
+  instagram: string
+  mainLink?: any
+  twitter: string
+  vk: string
+  website?: any
+  youtube?: any
+}
+export type UserPhotos = {
+  large: string
+  small: string
+}
+
+export type MessageArgs = {
+  count?: number
+  page?: number
+  userId: number
+}
+
+export type MessageFriendType = {
+  error?: null | string
+  items: MessageFriendTypeItems[]
+  totalCount: number
+}
+export type MessageFriendTypeItems = {
+  addedAt: string
+  body: string
+  id: string
+  recipientId: number
+  senderId: number
+  senderName: string
+  translatedBody?: any | string
+  viewed: boolean
+}
