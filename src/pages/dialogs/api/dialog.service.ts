d7bfda7f99e91b2ef5439ce3cad85a30a2c895d4
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-21 21:57:17 +0300

    feat: change structure dialog

--- src/pages/dialogs/api/dialog.service.ts
+++ src/pages/dialogs/api/dialog.service.ts
@@ -1,53 +1,19 @@
 import { baseApi } from '@/app/services/baseApi'
-import { ResponseType } from '@/app/services/baseApi/baseApi.types'
 import {
   DialogType,
   MessageArgs,
   MessageFriendType,
   UserType,
 } from '@/pages/dialogs/api/dialog.types'
-import { CreateProfileArgs } from '@/pages/profile/api/profile.types'
 
 const dialogService = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
-      createProfile: builder.mutation<ResponseType, CreateProfileArgs>({
-        invalidatesTags: ['Dialogs'],
-        query: args => ({
-          body: args,
-          method: 'PUT',
-          url: 'profile',
-        }),
-      }),
-      createProfilePhoto: builder.mutation<ResponseType, { image: string }>({
-        invalidatesTags: ['Dialogs'],
-        query: args => ({
-          body: args.image,
-          method: 'PUT',
-          url: 'profile/photo',
-        }),
-      }),
-      createProfileStatus: builder.mutation<ResponseType, { status: string }>({
-        invalidatesTags: ['Dialogs'],
-        query: args => ({
-          body: args.status,
-          method: 'PUT',
-          url: 'profile/status',
-        }),
-      }),
       getDialogs: builder.query<DialogType[], void>({
         providesTags: ['Dialogs'],
         query: () => {
           return {
             url: `dialogs`,
-          }
-        },
-      }),
-      getDialogsById: builder.query<UserType, { userId: number }>({
-        providesTags: ['Dialogs'],
-        query: ({ userId }) => {
-          return {
-            url: `dialogs/${userId}`,
           }
         },
       }),
@@ -60,9 +26,18 @@ const dialogService = baseApi.injectEndpoints({
           }
         },
       }),
+      updateDialogsById: builder.mutation<UserType, { userId: number }>({
+        invalidatesTags: ['Dialogs'],
+        query: args => {
+          return {
+            body: args,
+            method: 'PUT',
+            url: `dialogs/${args.userId}`,
+          }
+        },
+      }),
     }
   },
 })
 
-export const { useGetDialogsByIdQuery, useGetDialogsQuery, useGetMessagesFriendQuery } =
-  dialogService
+export const { useGetDialogsQuery, useGetMessagesFriendQuery } = dialogService
