b47a4cd9963840db1619de50c622aaa4f9d8db31
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-25 20:27:23 +0300

    feat: add

--- src/pages/dialogs/ui/messageDialog/ui/messageDialogs.tsx
+++ src/pages/dialogs/ui/messageDialog/ui/messageDialogs.tsx
@@ -1,9 +1,7 @@
-import { ChangeEvent, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate, useParams } from 'react-router-dom'
 
-import { Button, TextField } from '@/components/ui'
-import { useSendMessageFriendMutation } from '@/pages/dialogs/api'
+import { AddMessage } from '@/pages/dialogs/ui/messageDialog/ui/addMessage/addMessage'
 import { MessageDialogBlock } from '@/pages/dialogs/ui/messageDialog/ui/messageDialogBlock/messageDialogBlock'
 import { MessageHeader } from '@/pages/dialogs/ui/messageDialog/ui/messageHeader/messageHeader'
 
@@ -13,31 +11,13 @@ export const MessageDialogs = () => {
   const { userId } = useParams()
   const { t } = useTranslation()
   const navigate = useNavigate()
-  const [newMessage, setNewMessage] = useState('')
-
-  const [sendMessage] = useSendMessageFriendMutation()
-
-  const handleTextareaChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setNewMessage(e.currentTarget.value)
-  const sendMessageHandler = () => {
-    if (!newMessage) {
-      return
-    }
-    sendMessage({ body: newMessage, userId: Number(userId) })
-    setNewMessage('')
-  }
 
   return (
     <div className={s.dialogsMessage}>
       <div className={s.message__block}>
         <MessageHeader />
         <MessageDialogBlock />
-        <div className={s.message__panel}>
-          <TextField onChange={handleTextareaChange} type={'text'} value={newMessage}></TextField>
-          <Button onClick={sendMessageHandler} variant={'primary'}>
-            {t('Send')}
-          </Button>
-        </div>
+        <AddMessage />
       </div>
       <div className={s.message__navigation}>
         <div className={s.navigation__top}>
