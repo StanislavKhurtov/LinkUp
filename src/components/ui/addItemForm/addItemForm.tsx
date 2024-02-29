351a152aa6f7a128dedded10cb6e5c8bc7f384a5
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-02-29 21:33:24 +0300

    fix:change structure addItemForm.tsx

--- src/components/ui/addItemForm/addItemForm.tsx
+++ src/components/ui/addItemForm/addItemForm.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, InputHTMLAttributes, KeyboardEvent, ReactNode, useState } from 'react'
 
-import clsx from 'clsx'
+import s from './addItemForm.module.scss'
 
 type Props = {
   addItem: (title: string) => void
@@ -12,9 +12,11 @@ type Props = {
 export const AddItemForm = React.memo((props: Props) => {
   const [newTitle, setNewTitle] = useState<string>('')
   const [error, setError] = useState<null | string>(null)
+  const [showButton, setShowButton] = useState<boolean>(false)
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setNewTitle(e.currentTarget.value)
+    setShowButton(e.currentTarget.value !== '')
   }
   const onKeypressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
     if (error !== null) {
@@ -29,26 +31,26 @@ export const AddItemForm = React.memo((props: Props) => {
     if (newTitle.trim() !== '') {
       props.addItem(newTitle.trim())
       setNewTitle('')
+      setShowButton(false)
     } else {
       setError('Title is required')
     }
   }
 
   return (
-    <div className={'AddItemForm'}>
+    <div className={s.AddItemForm}>
       <div className={'AddItemForm__label'}>{props.label}</div>
       <input
         {...props}
-        className={clsx('AddItemForm__input', { error: error })}
+        className={s.input}
         disabled={props.disabled}
         onChange={onChangeHandler}
         onKeyPress={onKeypressHandler}
         value={newTitle}
       />
-      <button className={'AddItemForm__btn'} disabled={props.disabled} onClick={addItemHandler}>
-        {props.trigger}
+      <button className={s.btn} disabled={props.disabled} onClick={addItemHandler}>
+        {showButton && props.trigger}
       </button>
-      {error && <div className={'error-message'}>{error}</div>}
     </div>
   )
 })
